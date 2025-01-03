56dd9203db6a95d66452cfdccb6ebfbdf673ffae
Adam Kay <thousand_leaves@hotmail.com>
2025-01-03 14:53:48 +0000

    adds retrieve route to router

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -1,11 +1,13 @@
 import health from '../routes/health.js'
+import retrieve from '../routes/retrieve.js'
 
 const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
       server.route([].concat(
-        health
+        health,
+        retrieve
       ))
     }
   }
